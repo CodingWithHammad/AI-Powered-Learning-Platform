138d9860a5ea801d0c5b710e2b738f9002e5bbbc
CodingWithHammad <codingwithhammad786@gmail.com>
2026-01-06 23:16:26 +0530

    Making other pages and also making code reuseablity better

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ import VapiAgent from './pages/VapiAgent'
 import VoiceAgent from './pages/VoiceAgent'
 
 
-
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || 'pk_test_your-clerk-key-here'
 
 if (!PUBLISHABLE_KEY) {
